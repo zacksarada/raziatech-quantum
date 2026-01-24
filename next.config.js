38475686fe9d41debc295a9d67f1e5537b98e466
RAZIA TECH <hamandongotongh@gmail.com>
2026-01-24 19:18:45 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 }
 
 module.exports = nextConfig
