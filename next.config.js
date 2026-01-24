85ca9e072d7de271608b6d268ce40129c6bff7e4
RAZIA TECH <hamandongotongh@gmail.com>
2026-01-24 18:02:32 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    domains: ['images.unsplash.com', 'avatars.githubusercontent.com'],
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 }
 
